6a134ca832b86c64be9a02fef151562d9627c515
Fourcan Abdullah <fourcan.abdullah69@myhunter.cuny.edu>
2024-08-04 19:44:41 -0400

    fixed firebase access issue

--- pantry-tracker/firebase.js
+++ pantry-tracker/firebase.js
@@ -8,12 +8,11 @@ import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_APIKEY,
   authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
-  projectId: process.env.NEXT_PUBLIC_NEXT_PUBLIC_PROJECTID,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID,
   storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
   appId: process.env.NEXT_PUBLIC_APPID
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
